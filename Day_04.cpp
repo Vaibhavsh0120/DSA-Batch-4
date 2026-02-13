57c9760d9d1090b3d3207eaed264a79300a591cb
Vaibhavsh0120 <vaibhavsh0120@gmail.com>
2026-02-13 12:38:44 +0530

    update Readme

--- Day_04.cpp
+++ Day_04.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+// Day_04_01
+// 11. Container With Most Water 
+int maxArea(int height[] , int n) {
+    int low = 0;
+    int high = n - 1;
+
+    int length = 0;
+    int curArea = 0;
+    int maxArea = 0;
+
+    while (low < high) {
+
+        length = high - low;
+
+        curArea = min(height[low], height[high]) * length;
+
+        maxArea = max(curArea, maxArea);
+
+        if(height[low] < height[high]) {
+            low++;
+            continue;
+        }
+        else if (height[low] > height[high]) {
+            high--;
+            continue;
+        }
+        else {
+            low++;
+            high--;
+        }
+    }
+
+    return maxArea;
+}
+
+int main() {
     
     return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+Made By: Vaibhav Sharma
+CSE - Batch 4
+
 [DSA Training Progress](https://docs.google.com/spreadsheets/d/14ip1Tx68uwb8E1GQkQANKdpdtr7Vrlj3e9RHSFIRXQg/edit?usp=sharing)
