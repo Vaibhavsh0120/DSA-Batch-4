bcf82faa18a5f93e836efce2f677f0d143ff49ed
Vaibhavsh0120 <vaibhavsh0120@gmail.com>
2026-02-14 11:57:59 +0530

    Day 04

--- Day_04.cpp
+++ Day_04.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
 // Day_04_01
@@ -34,6 +35,56 @@ int maxArea(int height[] , int n) {
     }
 
     return maxArea;
+}
+
+// Day_04_02
+// HashMap Working
+void HasMap() {
+    unordered_map<int, int> mp;
+    /*
+        Insert → O(1)
+        Search → O(1)
+        Delete → O(1)
+    */
+
+    // mp[key] = value;        // insert or update
+    mp[1] = 10;
+    mp[2] = 20;
+
+    mp.insert({3, 30}); // IGNORED BY COMPILER OF ALREADY EXIST
+
+    // Check if Key Exists
+    if(mp.find(2) != mp.end())
+        cout << "Found";
+
+    // Count (Very Important)
+    // mp.count(key); 
+    // Returns:
+    // 1 → key exists
+    // 0 → not exists
+        
+    if(mp.count(5)) {
+        cout << "5 Exists\n";
+    } else {
+        cout << "5 DO NOT Exists\n";
+    }
+
+    // Erase
+    // mp.erase(key);
+
+    mp.erase(2);
+
+    mp.size();
+
+    mp.empty();
+
+    mp.clear();
+
+    for(auto &it : mp) { // Key Value Pair goes to IT
+        cout << it.first << " " << it.second << endl;
+    }
+
+    return;
 }
 
 int main() {
